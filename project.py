1787b3bd547e622ad2bb4bd03b465e8536489b01
Uma <pravallikauma5@gmail.com>
2025-08-17 10:50:50 +0530

    Full project committed

--- project.py
+++ project.py
@@ -1,10 +1,30 @@
 # Creating a social media working framework
 class chatbook : 
+    __user_id=1
     def __init__(self):
+        self.id=chatbook.__user_id
+        chatbook.__user_id+=1
+        self.__name = "Default User"
         self.username=''
         self.password=''
         self.loggedin=False
         self.menu()
+    @staticmethod
+    def get_id():
+        return chatbook.__user_id
+    
+    @staticmethod
+    def set_id(val):
+        chatbook.__user_id = val
+
+
+    def get_name(self):
+        return self.__name
+    
+
+    def set_name(self, value):
+        self.__name = value
+
     def menu(self):
         user_input=input("""Welcome to Chatbook || How would you like to proceed?
               1.Press 1 to signup
@@ -13,13 +33,57 @@ class chatbook :
               4.Press 4 to message a friend
               5.Press any other key to exit : """)
         if user_input=='1':
-            pass
+            self.signup()
         elif user_input=='2':
-            pass
+            self.signin()
         elif user_input=='3':
-            pass
+            self.mypost()
         elif user_input=='4':
-            pass
+            self.sendmsg()
         else :
             exit()
+
+
+    def signup(self):
+        email=input("Enter your email address : ")
+        pwd=input("Enter your password : ")
+        self.username=email
+        self.password=pwd
+        print("You have signedup successfully !!!")
+        print("\n")
+        self.menu()
+
+    def signin(self):
+        if self.username=='' and self.password=='':
+            print("***Please signup first by pressing 1***")
+        else :
+            uname=input("Enter your email address : ")
+            pwd=input("Enter your email password : ")
+            if self.username==uname and self.password==pwd:
+                print("You have signed in successfully!!!")
+                self.loggedin=True
+            else:
+                print("***Incorrect credentials***")
+        print("\n")
+        self.menu()
+
+    def mypost(self):
+        if self.loggedin==True:
+            txt = input("Enter your message here -> ")
+            print(f"Following content has been posted -> {txt}")
+        else:
+            print("You need to signin first to post something...")
+        print("\n")
+        self.menu()
+    
+    def sendmsg(self):
+        if self.loggedin==True:
+            txt = input("Enter your message here -> ")
+            frnd = input("Whom to send the msg? -> ")
+            print(f"Your message has been sent to {frnd}")
+        else:
+            print("You need to signin first to post something...")
+        print("\n")
+        self.menu()
+        
 obj=chatbook()
